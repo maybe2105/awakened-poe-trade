e8ceb4f4caee3d1cc5adb68416210e2eed390cec
wisp <luannguyen210500@gmail.com>
2025-07-22 22:51:11 +0700

    package-able

--- main/build/script.mjs
+++ main/build/script.mjs
@@ -30,7 +30,7 @@ const mainContext = await esbuild.context({
   bundle: true,
   minify: !isDev,
   platform: 'node',
-  external: ['electron', 'uiohook-napi', 'electron-overlay-window'],
+  external: ['electron', 'electron-overlay-window', 'uiohook-napi'],
   outfile: 'dist/main.js',
   define: {
     'process.env.STATIC': (isDev) ? '"../build/icons"' : '"."',
